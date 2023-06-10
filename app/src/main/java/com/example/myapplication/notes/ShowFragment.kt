6fa2b42ee5e7a0c70a7f56e8d79a71e782dbbe66
Eslam Fahmy <74387512+eslamfahmy2@users.noreply.github.com>
2023-06-10 17:05:06 +0300

    finish Show fragment ui and logic

--- app/src/main/java/com/example/myapplication/notes/ShowFragment.kt
+++ app/src/main/java/com/example/myapplication/notes/ShowFragment.kt
@@ -1,10 +1,13 @@
 package com.example.myapplication.notes
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.myapplication.Database
@@ -16,27 +19,35 @@ import kotlinx.coroutines.launch
 
 
 class ShowFragment : Fragment() {
-
-
-
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+
         val view = inflater.inflate(R.layout.fragment_show, container, false)
+        val btnNavigateToAdd: Button = view.findViewById(R.id.btnNavigateToAdd)
         val recyclerView = view.findViewById<RecyclerView>(R.id.rv)
         val database = Database.getDataBase(this.requireContext().applicationContext)
+        val deleteAll: Button = view.findViewById(R.id.btnDeleteAll)
+
+        btnNavigateToAdd.setOnClickListener {
+            val navController = findNavController()
+            navController.navigate(R.id.action_showFragment2_to_addFragment2)
+        }
+
         CoroutineScope(Dispatchers.IO).launch {
             val data = database.userDao().getPosts()
-            val recyclerRoom = RecyclerRoom(data)
-            recyclerView.adapter=recyclerRoom
+            val recyclerRoom = RecyclerRoom(data, database)
+            recyclerView.adapter = recyclerRoom
         }
-    recyclerView.layoutManager= LinearLayoutManager(requireContext(),RecyclerView.VERTICAL,false)
+        recyclerView.layoutManager =
+            LinearLayoutManager(requireContext(), RecyclerView.VERTICAL, false)
 
+        deleteAll.setOnClickListener {
+            CoroutineScope(Dispatchers.IO).launch {
+                database.userDao().deleteAll(database.userDao().getPosts())
+            }
+        }
         return view
     }
-
-
 }
